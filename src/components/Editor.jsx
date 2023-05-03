296bef6229d08cb0e3a1a4ace893bee3989b0743
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-05-03 12:33:00 +0530

    Created Codepen clone

---
+++ src/components/Editor.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Controlled as ControlledEditor } from "react-codemirror2";
+// import '../index.css'
+import '../index.css';
+
+// import { Controlled } from 'react-codemirror2'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { modes } from 'codemirror';
+
+
+
+export default function Editor(props) {
+  const {
+    language,
+    displayName,
+    value,
+    onChange
+  } = props
+
+  function handleChange(editor, data, value) {
+    onChange(value)
+  }
+
+  const[open,setOpen] = useState(true)
+
+  return (
+    <div className={`editor-container${open? '' : 'collapsed'}`}>
+      <div className="editor-title">
+        {displayName}
+        <button onClick={()=> setOpen(prevOpen => !prevOpen)}>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true
+        }}
+      />
+    </div>
+  )
+}
